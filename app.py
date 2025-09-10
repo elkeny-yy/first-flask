fe1196c2fe66b1c242fe16c212fe60b10ab2c300
elkeny-yy <202251194@std.umk.ac.id>
2025-09-10 19:05:18 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Halaman utama
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route backend sederhana untuk memproses form
+@app.route('/proses', methods=['POST'])
+def proses():
+    nama = request.form.get('nama')
+    return render_template('hasil.html', nama=nama)
+
+if __name__ == '__main__':
+    app.run(debug=True)
